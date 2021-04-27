21daa550e46c7900ad586c2562be61690f308056
adizh <83157462+adizh@users.noreply.github.com>
2021-04-27 11:43:07 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {useSelector,useDispatch} from "react-redux";
+import {increment,decrement,reset} from "../actions";
+
+const App = () => {
+const counter=useSelector(s=>s.counter)
+    const dispatch=useDispatch()
+    return (
+        <div>
+            {counter}
+            <button onClick={()=>dispatch(increment(5))}>add</button>
+            <button onClick={()=>dispatch(decrement())}>decrement</button>
+            <button onClick={()=>dispatch(reset())}>reset</button>
+        </div>
+    );
+};
+
+export default App;
